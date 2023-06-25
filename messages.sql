61aa608ef18ccb0767085b2e970b2192b4ea21ff
Masami <msm-k0304@hotmail.com>
2023-06-25 23:16:36 +1200

    fixed one test case in Questin 3

--- messages.sql
+++ messages.sql
@@ -22,7 +22,7 @@ CREATE TABLE messages (
 -- Create a stored procedure in MySQL
 
 DELIMITER //
-CREATE PROCEDURE `GetMessageByCountryTest`(IN DepatureCountry CHAR(3), IN DepatureDateTime DATETIME)
+CREATE PROCEDURE `GetMessageByCountryDate`(IN DepatureCountry CHAR(3), IN DepatureDateTime DATETIME)
 BEGIN
   SELECT * 
   FROM messages
@@ -59,12 +59,14 @@ CALL GetMessageByCountryAndDateTime('NZL', '2023-05-30 12:00:00')
 ------------
 -- Create test cases for Question 1 in MySQL
 -- dummy data
-
-INSERT INTO messages(admin_title, country_code, text_message, start_date, end_date, type) 
-VALUES ('Manager', 'NZL', 'Kia Ora', '2023-06-24 12:00:00', NULL, 'A')
-VALUES ('Admin Test', 'AAA', 'Hello', '2023-04-22 12:00:00', NULL, 'A')
-VALUES ('Admin', 'NZL', 'Happy Holiday', '2023-12-24 12:00:00', '2023-01-10 12:00:00', 'B')
-VALUES ('Office Administrator', 'AUS', 'Gidday', '2023-05-24 12:00:00', NULL , 'A')
+INSERT INTO messages(admin_title, country_code, text_message, start_date, end_date, message_type) 
+VALUES ('Manager', 'NZL', 'Kia Ora', '2023-06-24 12:00:00', NULL, 'A'),
+  ('Admin Test', 'AAA', 'Hello', '2023-04-22 12:00:00', NULL, 'A'),
+  ('Office Administrator', 'AUS', 'Gidday', '2023-05-24 12:00:00', NULL , 'A'),
+  ('Admin Test', 'ANZ', 'Happy Mothers Day', '2023-05-05 12:00:00', '2023-05-15 12:00:00', 'B'),
+  ('Office Administrator', 'NZL', 'Happy Holiday', '2023-12-24 12:00:00', '2023-01-10 12:00:00', 'B'),
+  ('Office Administrator', 'AUS', 'Happy Holiday', '2023-12-24 12:00:00', '2023-01-05 12:00:00', 'B'),
+  ('Admin Test', 'AAA', 'Hi, There', '2023-06-01 12:00:00', NULL, 'A');
 
 
 -- Test Case1 - check data retrieval
@@ -82,8 +84,8 @@ INSERT INTO messages(admin_title, country_code, text_message, start_date, end_da
 VALUES ('Manager', 'AB', 'message brah brah brah', '2023-05-24 12:00:00', '2023-06-24 12:00:00', 'A')
 
 -- Test Case4 - Update country code in a single row
--- return (3, 'Admin', 'AUS', 'Happy Holiday', '2023-12-24 12:00:00', '2023-01-10 12:00:00', 'B')
-UPDATE messages SET country_code = 'AUS' WHERE id = 3;
+-- return (3, 'Admin', 'AUS', 'Happy Holiday', '2023-12-25 12:00:00', NULL, 'B')
+UPDATE messages SET start_date = '2023-05-25 12:00:00' WHERE id = 3;
 
 -- Test Case5 - Delete a single row
 -- Delete id=2 row 
@@ -91,9 +93,35 @@ DELETE FROM messages WHERE id = 2;
 
 
 -- Create test cases for Question 2 in MySQL
--- Test Case 5
+-- Test Case 6
 -- return ('Manager', 'NZL', 'Kia Ora', '2023-06-24 12:00:00', NULL, 'A')
-CALL GetMessageByCountryAndDateTime('NZL', '2023-06-25 12:00:00')
+CALL GetMessageByCountryDate('NZL', '2023-06-25 12:00:00')
 
+DELIMITER //
 
+CREATE TRIGGER `check_procedure_GetMessageByCountryTest`( 
+  IN DepatureCountry CHAR(3), 
+  IN DepatureDateTime DATETIME
+)
 
+  BEGIN
+    -- Call the stored procedure
+    CALL GetMessageByCountryDate(DepatureCountry, DepatureDateTime)
+
+    -- Check if the update was successful
+    IF ROW_COUNT() = 0 THEN
+      -- Handle assertion failure
+      SELECT 'Assertion check_procedure_GetMessageByCountryTest failed: No message is returned '
+    END IF;
+
+    -- Check if the update was successful
+    IF ROW_COUNT() > 1  THEN
+      -- Handle assertion failure
+      SELECT 'Assertion check_procedure_GetMessageByCountryTest failed: multiple messages are returned '
+    END IF;
+    
+    -- Successful update
+    SELECT 'Single message is returned';
+  END
+
+DELIMITER ;
