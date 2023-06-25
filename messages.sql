a1a39d43d0d3f2e9c0d646b15db2d34feedfc058
Masami <msm-k0304@hotmail.com>
2023-06-25 22:28:50 +1200

    fixed question 2

--- messages.sql
+++ messages.sql
@@ -8,8 +8,8 @@ CREATE TABLE messages (
   country_code CHAR(3) NOTNULL,
   text_message TEXT NOTNULL,
   start_date DATETIME NOTNULL,
-  end_date DATETIME
-  CHAR(1) NOTNULL
+  end_date DATETIME,
+  message_type CHAR(1) NOTNULL
 );
 
 
@@ -22,63 +22,32 @@ CREATE TABLE messages (
 -- Create a stored procedure in MySQL
 
 DELIMITER //
-CREATE PROCEDURE `GetMessageByCountryAndDateTime`(IN DepatureCountry CHAR(3), IN DepatureDateTime DATETIME)
+CREATE PROCEDURE `GetMessageByCountryTest`(IN DepatureCountry CHAR(3), IN DepatureDateTime DATETIME)
 BEGIN
-  SELECT * FROM messages
+  SELECT * 
+  FROM messages
   WHERE country_code = DepatureCountry
-    AND start_date <= DepatureDateTime
-    AND end_date >= DepatureDateTime;
+	AND start_date <= DepatureDateTime
+    AND end_date >= DepatureDateTime
+    AND message_type = 'B'
+  UNION
+  SELECT * 
+  FROM messages
+  WHERE country_code = DepatureCountry
+	AND start_date<= DepatureDateTime
+    AND message_type = 'A'
+  UNION
+  SELECT * 
+  FROM messages
+  WHERE country_code = 'AAA'
+	AND start_date<= DepatureDateTime
+  ORDER BY country_code DESC, message_type DESC, start_date DESC
+  LIMIT 1;
 END
 DELIMITER ;
 
 -- Example call
 CALL GetMessageByCountryAndDateTime('NZL', '2023-05-30 12:00:00')
-
-
-DELIMITER //
-CREATE PROCEDURE `GetMessageByCountryAndDateTime`(IN DepatureCountry CHAR(3), IN DepatureDateTime DATETIME)
-BEGIN
-  SELECT * FROM messages
-  WHERE country_code = DepatureCountry
-    AND start_date <= DepatureDateTime
-    AND end_date >= DepatureDateTime;
-END
-DELIMITER ;
-
--- DELIMITER //
--- CREATE PROCEDURE GetMessageByCountryAndDateTime (IN DepatureCountry CHAR(3), IN DepatureDateTime DATETIME)
---   BEGIN
---     DECLARE end_date_val DATETIME
---       SELECT end_date INTO end_date_val FROM messages WHERE country_code = DepatureCountry
-
---     IF end_date_val IS NULL THEN
---       SELECT * FROM messages
---       WHERE country_code = DepatureCountry
---       AND start_date <= DepatureDateTime
---     ELSE
---       SELECT * FROM messages
---       WHERE country_code = DepatureCountry
---       AND start_date <= DepatureDateTime
---       AND end_date >= DepatureDateTime;
---     END IF;
--- END;
--- DELIMITER ;
-
--- CREATE PROCEDURE `GetMessageByCountryAndDateTime` (IN DepatureCountry CHAR(3), IN DepatureDateTime DATETIME)
---   BEGIN
---     CASE
---       WHEN end_date IS NULL THEN
---           SELECT * FROM messages
---           WHERE country_code = DepatureCountry
---           AND start_date <= DepatureDateTime;
---       ELSE
---           SELECT * FROM messages
---           WHERE country_code = DepatureCountry
---           AND start_date <= DepatureDateTime
---           AND end_date >= DepatureDateTime;
---     END CASE;
--- END;
--- DELIMITER ;
 
 
 
@@ -89,49 +58,42 @@ DELIMITER ;
 --Question3 
 ------------
 -- Create test cases for Question 1 in MySQL
+-- dummy data
+
+INSERT INTO messages(admin_title, country_code, text_message, start_date, end_date, type) 
+VALUES ('Manager', 'NZL', 'Kia Ora', '2023-06-24 12:00:00', NULL, 'A')
+VALUES ('Admin Test', 'AAA', 'Hello', '2023-04-22 12:00:00', NULL, 'A')
+VALUES ('Admin', 'NZL', 'Happy Holiday', '2023-12-24 12:00:00', '2023-01-10 12:00:00', 'B')
+VALUES ('Office Administrator', 'AUS', 'Gidday', '2023-05-24 12:00:00', NULL , 'A')
+
 
 -- Test Case1 - check data retrieval
-SELECT * FROM message;
+-- Return 2 rows
 SELECT * FROM message WHERE country-code = 'NZL';
 
 -- Test Case2 - test a single row insertion
+-- Insert one row
 INSERT INTO messages(admin_title, country_code, text_message, start_date, end_date, type) 
-VALUES ('Manager', 'NZL', 'message brah brah brah', '2023-05-24 12:00:00', '2023-06-24 12:00:00')
+VALUES ('Office Administrator', 'NZL', 'Happy New Year', '2024-01-01 12:00:00', '2024-01-05 12:00:00', 'B')
 
 -- Test Case3 - check invalid country code
--- False
+-- Return error
 INSERT INTO messages(admin_title, country_code, text_message, start_date, end_date, type) 
-VALUES ('Manager', 'AB', 'message brah brah brah', '2023-05-24 12:00:00', '2023-06-24 12:00:00')
+VALUES ('Manager', 'AB', 'message brah brah brah', '2023-05-24 12:00:00', '2023-06-24 12:00:00', 'A')
 
 -- Test Case4 - Update country code in a single row
-UPDATE messages SET country_code = 'AUS' WHERE id = 1;
+-- return (3, 'Admin', 'AUS', 'Happy Holiday', '2023-12-24 12:00:00', '2023-01-10 12:00:00', 'B')
+UPDATE messages SET country_code = 'AUS' WHERE id = 3;
 
 -- Test Case5 - Delete a single row
-DELETE FROM messages
-WHERE id = 1;
+-- Delete id=2 row 
+DELETE FROM messages WHERE id = 2;
 
 
 -- Create test cases for Question 2 in MySQL
-
--- Test Case
-CREATE PROCEDURE GetEmployeeCount(IN departmentId INT, OUT employeeCount INT)
-BEGIN
-  IF departmentId IS NULL THEN
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Department ID cannot be null';
-  END IF;
-
-  SELECT COUNT(*) INTO employeeCount FROM Employees WHERE department_id = departmentId;
-END;
-
--- ??
-
-DELIMITER //
-CREATE TRIGGER assertion_trigger
-BEFORE INSERT ON messages
-FOR EACH ROW
-BEGIN
-  IF NEW.start_date >= 
+-- Test Case 5
+-- return ('Manager', 'NZL', 'Kia Ora', '2023-06-24 12:00:00', NULL, 'A')
+CALL GetMessageByCountryAndDateTime('NZL', '2023-06-25 12:00:00')
 
 
-END;
 
